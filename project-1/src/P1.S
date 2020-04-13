f86540635f3255104818d411997359a948232b42
zjnett <zjnett@mac.com>
2020-04-13 11:29:21 -0700

    Uploaded project 1 and 2 code

---
+++ project-1/src/P1.S
@@ -0,0 +1,18 @@
+.text
+.global main
+
+@define constants, these can be used as symbols in your code
+.equ LED_CTL, 0x41210000
+.set SW_DATA, 0x41220000
+
+@the set and equ directives are equivalent and can be used interchangeably
+
+main:
+	ldr r1,=SW_DATA	@load switch address from constant
+	ldr r2,=LED_CTL	@load LED address from constant
+loop:
+	ldr r0,[r1]	@load switch value *r1 ->r0
+	str r0,[r2]	@store value to led register *r2 <-r0
+	b loop		@go back to "loop"
+
+.end
